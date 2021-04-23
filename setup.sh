950c015db4587f2f773cc64bdb39e9aeb7e44322
The H.I.V.E Project <80300015+thehiveproject@users.noreply.github.com>
2021-04-23 15:22:17 +1200

    fixed order of installation

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
+echo 'Thank you for downloading MMM-H.I.V.E. This is a subproject of our main project, The H.I.V.E Project. All Dependencies will shortly be downloaded. If there is an issue, please lodge an issue at Github.com/NateBrownProjects/MMM-Hive. Thank you'
+pip3 install virtualenv
+sudo virtualenv TheHiveProject
 pip3 install SpeechRecognition
 pip3 install pyttsx3
 pip3 install pywhatkit
@@ -9,6 +12,5 @@ pip3 install math
 pip3 install requests
 pip3 install owm
 pip3 install wx
-pip3 install virtualenv
-sudo virtualenv TheHiveProject
 rm -r setup.sh
+echo 'The Instalation Procces is complete.'
